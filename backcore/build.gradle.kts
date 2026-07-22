4a640ee53b35f47b761d2c1ae6012e6ae1a45fe3
boyninja1555 <flappyfloorg@gmail.com>
2026-07-22 14:35:45 -0500

    Enforcing Java 25 in backcore plugin

--- backcore/build.gradle.kts
+++ backcore/build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
     id("com.gradleup.shadow") version "9.6.1"
 }
 
+java.toolchain.languageVersion.set(JavaLanguageVersion.of(25))
+
 repositories {
     mavenCentral()
     maven("https://mvn.flappygrant.com")
